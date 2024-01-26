43f3499273b7d51ee04576f752351c81e6c9abe3
JuanmaMesa <juanma_sme@hotmail.com>
2024-01-26 20:56:23 +0100

    Añadiendo campos de formulario en  add

--- S05T01N01SanchezMesa/S05T01N01SanchezMesa/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n01/S05T01N01/controller/BankBranchController.java
+++ S05T01N01SanchezMesa/S05T01N01SanchezMesa/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n01/S05T01N01/controller/BankBranchController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class BankBranchController {
 
     }
 
-    @GetMapping("/add")
+    @GetMapping ("/add")
     public String addNewBranch( Model model){
         BankBranch bankBranch = new BankBranch();
         model.addAttribute("branch", bankBranch);
