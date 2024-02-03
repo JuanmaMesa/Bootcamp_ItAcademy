eb69887408777ddf9e373ea09156b2b6c8286f30
JuanmaMesa <juanma_sme@hotmail.com>
2024-02-03 17:14:46 +0100

    empezando proyecto

---
+++ S05T01N02SanchezMesaJuanManuel/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n02/S05T01N02/model/domain/FlorEntity.java
@@ -0,0 +1,8 @@
+package cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel.s05.t01.n02.S05T01N02.model.domain;
+
+public class FlorEntity {
+
+    private Integer pk_FlorID;
+    private String nomFlor;
+    private String paisFlor;
+}
