5bf1cecb6cc6c0275f8be867a82753c291f32c41
JuanmaMesa <juanma_sme@hotmail.com>
2024-02-07 10:43:43 +0100

    empezando el proyecto

---
+++ src/test/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/JuegosDeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegosDeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
