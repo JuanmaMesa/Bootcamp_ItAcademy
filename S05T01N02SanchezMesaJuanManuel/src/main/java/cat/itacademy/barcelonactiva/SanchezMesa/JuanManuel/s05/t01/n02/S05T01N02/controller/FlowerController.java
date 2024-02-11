23ea5ca512a043ecac9a8b262f373ecf58335a0b
JuanmaMesa <juanma_sme@hotmail.com>
2024-02-11 08:01:12 +0100

    documentacion completada

--- S05T01N02SanchezMesaJuanManuel/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n02/S05T01N02/controller/FlowerController.java
+++ S05T01N02SanchezMesaJuanManuel/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n02/S05T01N02/controller/FlowerController.java
@@ -4,13 +4,15 @@ import cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel.s05.t01.n02.S05T01N02
 import cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel.s05.t01.n02.S05T01N02.model.dto.FlowerDto;
 import cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel.s05.t01.n02.S05T01N02.model.dto.FlowerMapper;
 import cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel.s05.t01.n02.S05T01N02.model.services.FlowerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Tag(name = "Flower", description = "Flower management APIs")
 @RestController
 //@RequestMapping("/api/v1")
 public class FlowerController {
@@ -18,11 +20,19 @@ public class FlowerController {
     @Autowired
     private FlowerService service;
 
+    @Operation(
+            summary  = "Find all Flowers",
+            description = "Find all Flowers. List of all objects with id, name country and the type of Euro or Not Euro Country",
+            tags = {"get"}
+    )
     @GetMapping("/findAll")
     public List<FlowerDto> flowerEntityList(){
         return service.getAllFlower();
     }
-
+    @Operation(
+            summary = "Create a Flower",
+            description = "Create a Flower object. The response is a httpStatus showing the result of the operation.",
+            tags = {"post"})
     @PostMapping("/add")
     public ResponseEntity<FlowerDto> addFlower(@RequestBody FlowerDto flowerDto){
         FlowerDto newFlowerDto = service.addFlower(flowerDto);
