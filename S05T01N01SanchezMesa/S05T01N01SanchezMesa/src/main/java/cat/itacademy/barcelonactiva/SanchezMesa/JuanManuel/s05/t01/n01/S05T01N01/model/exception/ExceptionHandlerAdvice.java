870fe308989524d42151c73608be9f47b2b825b2
JuanmaMesa <juanma_sme@hotmail.com>
2024-02-12 11:12:59 +0100

    errorMessage

--- S05T01N01SanchezMesa/S05T01N01SanchezMesa/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n01/S05T01N01/model/exception/ExceptionHandlerAdvice.java
+++ S05T01N01SanchezMesa/S05T01N01SanchezMesa/src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/s05/t01/n01/S05T01N01/model/exception/ExceptionHandlerAdvice.java
@@ -1,8 +1,13 @@
 package cat.itacademy.barcelonactiva.SanchezMesa.JuanManuel.s05.t01.n01.S05T01N01.model.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
