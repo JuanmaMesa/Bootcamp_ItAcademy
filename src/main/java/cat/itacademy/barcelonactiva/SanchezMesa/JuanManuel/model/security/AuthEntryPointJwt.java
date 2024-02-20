e7c4310c92bbaa65a89f12c5a3f435b0a0030cb5
JuanmaMesa <juanma_sme@hotmail.com>
2024-02-20 10:11:41 +0100

    ultima revision

--- src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/model/security/AuthEntryPointJwt.java
+++ src/main/java/cat/itacademy/barcelonactiva/SanchezMesa/JuanManuel/model/security/AuthEntryPointJwt.java
@@ -9,6 +9,19 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
